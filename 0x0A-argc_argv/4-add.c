5c3b0ceda85cdac118f27419738c4b24f906822f
Omar-debuging <eladnaniomar153@gmail.com>
2023-09-03 07:30:25 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,10 +14,10 @@ int check_num(char *str)
 	unsigned int count;
 
 	count = 0;
-	while (count < strlen(str)) 
+	while (count < strlen(str))
 
 	{
-		if (!isdigit(str[count])) 
+		if (!isdigit(str[count]))
 		{
 			return (0);
 		}
@@ -43,12 +43,12 @@ int main(int argc, char *argv[])
 	int sum = 0;
 
 	count = 1;
-	while (count < argc) 
+	while (count < argc)
 	{
 		if (check_num(argv[count]))
 
 		{
-			str_to_int = atoi(argv[count]); 
+			str_to_int = atoi(argv[count]);
 			sum += str_to_int;
 		}
 		else
