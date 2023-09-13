adb080b9460e316a38ce06629dae90449492674c
Omar-debuging <eladnaniomar153@gmail.com>
2023-09-13 13:45:38 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog's information
+ * @name: First name
+ * @age: his age
+ * @owner: his owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
